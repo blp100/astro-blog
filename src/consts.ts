e2b0e6581f4c2a77b99de3a5dc4b3c81bd2a08e3
Po-Cheng Yeh <blp100x@hotmail.com>
2023-08-17 00:55:38 +0800

    fix(meta): update website info

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Code, Stories, and Game Nights";
+export const SITE_DESCRIPTION = "A Blog of Code, Books, and Board Game Stories which I've ever been through it.";
+export const HOMEPAGE_URL = "https://po-cheng-yeh.vercel.app/"
