0c72f202e3007a6bf063f87a52816c5a23efb0fb
Po-Cheng Yeh <blp100x@hotmail.com>
2023-09-08 16:40:14 +0800

    fix: text size and vercel deploy issue on shiki pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,17 @@ import addClasses from "rehype-add-classes";
 import markdownIntegration from "@astropub/md";
 import vercel from "@astrojs/vercel/serverless";
 
+const shikiResourcePaths = Object.keys(
+  import.meta.glob([
+    "./node_modules/.pnpm/shiki@*/node_modules/shiki/languages/*.tmLanguage.json",
+    "./node_modules/.pnpm/shiki@*/node_modules/shiki/themes/*.json",
+  ])
+);
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({ includeFiles: shikiResourcePaths }),
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), react(), markdownIntegration()],
   markdown: {
