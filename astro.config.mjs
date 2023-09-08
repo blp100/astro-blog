f4258f7d1095c269dbe32a14297861b70230fe3b
Po-Cheng Yeh <blp100x@hotmail.com>
2023-09-08 15:37:39 +0800

    fix: ssr with react-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,4 +30,10 @@ export default defineConfig({
       ],
     ],
   },
+  vite: {
+    ssr: {
+      // Example: Force a broken package to skip SSR processing, if needed
+      external: ["react-icons"],
+    },
+  },
 });
