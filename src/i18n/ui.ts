8d1aa48a0b2e349dbe0e6d524cf75f8c70636991
Po-Cheng Yeh <blp100x@hotmail.com>
2023-09-10 00:32:25 +0800

    feat: add language detection

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  "zh-tw": "中文",
+};
+
+export const defaultLang = "zh-tw";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.categories": "Categories",
+  },
+  "zh-tw": {
+    "nav.home": "首頁",
+    "nav.about": "關於",
+    "nav.categories": "目錄",
+  },
+} as const;
