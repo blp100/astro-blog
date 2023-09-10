60459662621aa6c654296c6841a3ee9bfaa0a22d
Po-Cheng Yeh <blp100x@hotmail.com>
2023-09-10 22:55:54 +0800

    fix: deploy error after remove content repo

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,15 +3,15 @@ import { getCollection } from 'astro:content';
 import { contentfulClient } from "../lib/contentful";
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
-}
+// export async function get(context) {
+// 	const posts = await getCollection('blog');
+// 	return rss({
+// 		title: SITE_TITLE,
+// 		description: SITE_DESCRIPTION,
+// 		site: context.site,
+// 		items: posts.map((post) => ({
+// 			...post.data,
+// 			link: `/blog/${post.slug}/`,
+// 		})),
+// 	});
+// }
