93ce7dc37f7eda44f933f839f4d5bed73f641440
Po-Cheng Yeh <blp100x@hotmail.com>
2023-09-10 18:05:29 +0800

    feat: update new post style

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,12 +18,12 @@ export default defineConfig({
         addClasses,
         {
           h1: "text-4xl font-bold scroll-mt-16",
-          h2: "text-2xl font-bold scroll-mt-16",
-          h3: "text-xl font-bold scroll-mt-16",
+          h2: "text-2xl font-bold scroll-mt-16 text-boston-blue-700 dark:text-boston-blue-200",
+          h3: "text-xl font-bold scroll-mt-16 text-boston-blue-500",
           h4: "text-lg font-bold",
           h5: "font-bold",
           h6: "font-bold",
-          hr: "border-t-[6px] w-52 mx-auto my-4 border-orange-200 dark:border-zinc-400 border-double",
+          hr: "border-t-[1rem] w-52 mx-auto my-4 border-orange-200 dark:border-zinc-400 border-double",
           p: "text-lg my-2",
           blockquote:
             "p-4 bg-amber-100 dark:bg-neutral-800 border-l-4 border-orange-200 dark:border-neutral-700",
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,22 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        ivory: "#fefffa",
+        "boston-blue": {
+          50: "#f0f9fb",
+          100: "#daeef3",
+          200: "#b8dee9",
+          300: "#88c6d8",
+          400: "#50a4c0",
+          500: "#3a98b9",
+          600: "#2e6f8c",
+          700: "#2b5b73",
+          800: "#2a4d60",
+          900: "#274152",
+          950: "#152937",
+        },
+      },
       fontFamily: {
         righteous: ["'Righteous'", "cursive", "sans-serif"],
         notoSans: ["'Noto Sans TC'", "Verdana", "sans-serif"],
