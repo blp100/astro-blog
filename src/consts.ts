b617a9a8065de7903e4fa3f3a21a832421bd40dd
Po-Cheng Yeh <blp100x@hotmail.com>
2023-09-14 20:45:23 +0800

    fix: change site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Code, Stories, and Game Nights";
+export const SITE_TITLE = "Code, Stories, & Game Nights";
 export const SITE_DESCRIPTION =
   "Step into my realm, where code, books, and board games weave captivating tales of imagination.";
 export const HOMEPAGE_URL = "https://po-cheng-yeh.vercel.app/";
