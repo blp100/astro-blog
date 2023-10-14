76a2878870d9a0ef5c7c5e24b2585d5c00c0910e
Po-Cheng Yeh <blp100x@hotmail.com>
2023-10-14 14:41:18 +0800

    fix: paginaiton size in Page

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,4 @@ export const SITE_TITLE = "Code, Stories, & Game Nights";
 export const SITE_DESCRIPTION =
   "Step into my realm, where code, books, and board games weave captivating tales of imagination.";
 export const HOMEPAGE_URL = "https://po-cheng-yeh.vercel.app/";
+export const PAGINATIOM_SIZE = 3;
